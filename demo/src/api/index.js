1999110a966aa4ed79e0d76fa2bf3218213a4040
TinkleDing <dingyuliang>
2019-11-04 09:30:47 +0800

    后台接口地址

--- demo/src/api/index.js
+++ demo/src/api/index.js
@@ -1,2 +1,2 @@
-const api='http://192.168.1.143:3333/api/'
+const api='http://localhost:3333/api/'
 export default api;
--- demo/src/axios/index.js
+++ demo/src/axios/index.js
@@ -4,14 +4,15 @@ import axios from 'axios'
 axios.interceptors.request.use(function (config) {
   // Do something before request is sent
   //window.localStorage.getItem("accessToken") 获取token的value
-  let token = store.getState().user.value.token
-
+  let token
+  token = JSON.stringify(store.getState()).length>2? store.getState().user.value.token :''
   if (token) {
       //将token放到请求头发送给服务器,将tokenkey放在请求头中
       config.headers.accessToken = token;     
       config.headers.Authorization = token;     
-       return config;
   }
+  return config;
+
 }, function (error) {
   // Do something with request error
   return Promise.reject(error);
