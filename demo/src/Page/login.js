e4329f76be149c8c6ccc8e98b29666f3476ff6ce
dingyuliang <yuliang.yu@maiunsoft.com>
2019-11-05 18:01:44 +0800

    '加东西'

--- demo/src/Page/login.js
+++ demo/src/Page/login.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import '../App.css';
+import '../css/login.scss';
 import axios from 'axios';
 import {Button,Input,message} from 'antd';
 
 function Login() {
-    return (<div>1</div>)
+    return (<div className='login-background'>
+        <div className='login-box'>1</div>
+    </div>)
 }
 
 export default Login;
