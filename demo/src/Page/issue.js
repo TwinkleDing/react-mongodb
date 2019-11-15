801fcee4362886ae40bab468aa9f6ad0646f7877
dingyuliang <yuliang.yu@maiunsoft.com>
2019-11-15 14:16:39 +0800

    '修改'

--- demo/src/Page/issue.js
+++ demo/src/Page/issue.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 function Issue(){
     return(<div>
-        <div>登录拦截的时候的路由跳转</div>
-        <div>axios请问的错误拦截</div>
-        <div>留言列表添加留言之后第一次添加的component提示问题</div>
-        <div>文件上传返回url后台问题</div>
-        <div></div>
+        <ol>
+            <li>快捷创建react的class是rcc</li>
+            <li>登录拦截的时候的路由跳转</li>
+            <li>axios请问的错误拦截</li>
+            <li>留言列表添加留言之后第一次添加的component提示问题</li>
+            <li>文件上传返回url后台问题</li>
+            <li>只有使用router组件渲染的组件才props具有router的api，使用withRouter包裹起来</li>
+            <li>reducer和action可分开写</li>
+            <li>之后用readux写</li>
+        </ol>
     </div>)
 }
 
