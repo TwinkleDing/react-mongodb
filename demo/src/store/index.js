7edef81ed914c39383a56a95fde07c1eccf68d34
dingyuliang <yuliang.yu@maiunsoft.com>
2019-12-06 09:55:09 +0800

    '12yue'

--- demo/src/store/index.js
+++ demo/src/store/index.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk'
+// import thunk from 'redux-thunk'
 
 import reducer from './reducer'
-const store = createStore(reducer,applyMiddleware(thunk));
+const store = createStore(reducer);
+// const store = createStore(reducer,applyMiddleware(thunk));
 export default store;
